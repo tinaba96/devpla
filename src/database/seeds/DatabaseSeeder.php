adca7d70b271bee3f4c94aaf6d2ac4d2627b4e32
tinaba96 <tinaba178.96@gmail.com>
2021-06-02 00:32:14 +0900

    changed for the workbench

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(PostsTableSeeder::class);
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
