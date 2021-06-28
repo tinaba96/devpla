aa9d6da071fcf5f9a4fe5dd3cb4ee1bf759483c8
revoiot <revowater@gmail.com>
2021-06-29 00:25:25 +0900

    絵文字機能修正

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -11,11 +11,12 @@
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
         crossorigin="anonymous"
     >
-
+    <link href="{{ asset('css/view.css') }}" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
     <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -23,35 +24,16 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    <script src="simplemde-with-emoji-picker-main/simplemde-markdown-editor-master/simplemde.min.js"></script>
-    <link rel="stylesheet" href="simplemde-with-emoji-picker-main/simplemde-markdown-editor-master/simplemde.min.css">
-
-    <script src="simplemde-with-emoji-picker-main/unicode-emoji-picker/js/config.js"></script>
-    <script src="simplemde-with-emoji-picker-main/unicode-emoji-picker/js/emoji-picker.js"></script>
+    <!-- 絵文字機能 移動削除厳禁-->
     <link href="simplemde-with-emoji-picker-main/unicode-emoji-picker/css/emoji.css" rel="stylesheet">
-    
-    <link href="{{ asset('css/view.css') }}" rel="stylesheet">
+    <link href="simplemde-with-emoji-picker-main/unicode-emoji-picker/css/emoji_add.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
+    <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+    <!-- 絵文字機能 移動削除厳禁-->
 
-    <style>
-        .editor-toolbar:hover {
-            opacity: unset !important;
-        }
 
-        .editor-toolbar {
-            opacity: 1;
-        }
-
-        .emoji-menu {
-            top: 100%;
-        }
-
-        @media only screen and (max-width: 768px) {
-            .emoji-menu {
-                left: 20px !important;
-            }
-        }
-    </style>
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
@@ -137,13 +119,12 @@
         @yield('content')
     </div>
 
+<!-- 絵文字機能 移動削除厳禁-->
 <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-
-<script>
-    var simplemde = new SimpleMDE({ element: document.getElementById("editor") });
-</script>
-
-
+<script src="simplemde-with-emoji-picker-main/unicode-emoji-picker/js/emoji_add.js"></script>
+<script src="simplemde-with-emoji-picker-main/unicode-emoji-picker/js/config.js"></script>
+<script src="simplemde-with-emoji-picker-main/unicode-emoji-picker/js/emoji-picker.js"></script>
+<!-- 絵文字機能 移動削除厳禁-->
 
     @if(Auth::check())
     <script>
