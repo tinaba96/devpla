5ee49539be6cb3337df45a958ee2ad0463833324
revoiot <revowater@gmail.com>
2021-08-08 23:54:44 +0900

    ユーザ詳細遷移できるように修正

--- src/resources/views/chatgroup_members.blade.php
+++ src/resources/views/chatgroup_members.blade.php
@@ -12,43 +12,31 @@
 </style>
 
 <div class="container">
-    <h1 style="color:green; text-align:center;">チャットメンバー</h1>
-            @if (!Auth::user()->user_chatgroup()->where('chatgroup_id', $chatgroup->id)->exists() )
-                <form action='/homechat/{{ $chatgroup->id }}/bemember' method='POST'>
-                @csrf
-                <button type='submit' class='btn btn-primary'> メンバーになる </button>
-                </form>
-            @endif
+    <h1 class="font-medium text-white text-center ">チャット参加者一覧</h1>
 </div>
 
-
-<div class="flex flex-col items-center">
 @foreach($members as $member)
-@foreach($groups as $group)
+<a href="{{ url('/users/'. $member->users()->first()->id) }}" class="flex flex-col items-center text-black">
     <div class="w-full md:w-1/2 flex flex-col items-center h-10">
         <div class="w-full px-4">
             <div class="flex flex-col items-center relative">
                 <div class="absolute shadow bg-white top-100 z-40 w-full lef-0 rounded max-h-select overflow-y-auto svelte-5uyqqj">
                     <div class="flex flex-col w-full">
                         <div class="cursor-pointer w-full border-gray-100 rounded-t border-b hover:bg-teal-100">
+
                             <div class="flex w-full items-center p-2 pl-2 border-transparent border-l-2 relative hover:border-teal-100">
                                 <div class="w-6 flex flex-col items-center">
-
                                     <div class="flex relative w-5 h-5 bg-orange-500 justify-center items-center m-1 mr-2 w-4 h-4 mt-1 rounded-full ">
 
-
-                                        @if($group->id == $member->chatgroup_id)
-                                        <img src="{{ $member->users()->first()->profile_image }}" width="48" height="48" class="rounded-full" />
-                                        @endif
+                                            <img src="{{ $member->users()->first()->profile_image }}" width="48" height="48" class="rounded-full" />
 
                                     </div>
                                 </div>
-
                                 <div class="w-full items-center flex">
-                                    <div class="mx-2 -mt-1  "> {{ $member->users()->first()->name }}
-                                        <div class="text-xs truncate w-full normal-case font-normal -mt-1 text-gray-500">CEO &amp; managin director</div>
+                                    <div class="mx-2 -mt-1"> {{ $member->users()->first()->name }}
                                     </div>
                                 </div>
+
                             </div>
                         </div>
                     </div>
@@ -56,8 +44,16 @@
             </div>
         </div>
     </div>
-    @endforeach
-    @endforeach
+</div>
+</a>
+@endforeach
+<div class="flex flex-col justify-center items-center h-20">
+    @if (!Auth::user()->user_chatgroup()->where('chatgroup_id', $chatgroup->id)->exists() )
+        <form action='/homechat/{{ $chatgroup->id }}/bemember' method='POST'>
+        @csrf
+        <button type='submit' class='btn btn-primary'> メンバーになる </button>
+        </form>
+    @endif
 </div>
 
 @endsection
