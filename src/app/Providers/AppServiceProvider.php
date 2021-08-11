ffb4e45c4dc20d29e450a632cdcb04477256933e
tinaba96 <tinaba178.96@gmail.com>
2021-08-11 21:59:55 +0900

    update

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -24,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+      if ($this->app->environment() == 'production') {
+        URL::forceScheme('https');
+    }
       // グローバル変数
       // 管理者のID番号を1とする
       // 参照: https://stackoverflow.com/questions/28356193/
