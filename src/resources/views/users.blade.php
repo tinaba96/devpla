6b66b7c9448456160fba0741ca55b66f4316c7d0
revoiot <revowater@gmail.com>
2021-08-30 23:21:01 +0900

    フォローボタン修正

--- src/resources/views/users.blade.php
+++ src/resources/views/users.blade.php
@@ -40,7 +40,7 @@
                                         @else
                                             <form action='/users/{{ $user->id }}/follow' method='POST'>
                                             @csrf
-                                            <button type='submit' class='bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'> ＋ Follow</button>
+                                            <button type='submit' class='whitespace-nowrap bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'> ＋ Follow</button>
                                             </form>
                                         @endif
                                     @endif
